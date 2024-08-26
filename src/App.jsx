74920d1d6706e86ce9760b6c595ae8316b1b1f19
mcenerym <mike.mcenery1@gmail.com>
2024-08-26 12:52:38 -0400

    Added basic employee grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import employeeData from './data/employeesData.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const headerData = Object.keys(employeeData.reduce((o, c) => Object.assign(o, c)))
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className='bg-slate-800 grid w-full h-screen border-2 place-items-center'>
+			<div className='bg-zinc-800 w-1/2 h-screen'>
+				<table className='table-auto'>
+					<thead>
+						<tr>
+							{headerData &&
+								headerData.map((header, id) => (
+									<th key={id} className='bg-blue-700 border px-8 py-2'>
+										{header}
+									</th>
+								))}
+						</tr>
+					</thead>
+					<tbody>
+						{employeeData &&
+							employeeData.map((employee, id) => (
+								<tr key={id}>
+									{headerData &&
+										headerData.map((header, id) => (
+											<td key={id} className='bg-white border px-8 py-2'>
+												{employee[header]}
+											</td>
+										))}
+
+									{/* <td className='bg-white border px-8 py-2'>{employee.Title}</td>
+									<td className='bg-white border px-8 py-2'>{employee.Department}</td> */}
+								</tr>
+							))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
 }
 
 export default App
