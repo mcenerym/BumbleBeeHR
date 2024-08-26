e4a85ac03be996896ef4e08c2c18a41949b3470b
mcenerym <mike.mcenery1@gmail.com>
2024-08-26 14:37:43 -0400

    Project renamed to honeycomb hr

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import EmployeeDashboard from './components/EmployeeDashboard'
 function App() {
 	return (
 		<Routes>
-			<Route path='/BumbleBeeHR/' element={<EmployeeDashboard />} />
-			<Route path='/BumbleBeeHR/addEmployee' element={<AddEmployee />} />
+			<Route path='/HoneyCombHR/' element={<EmployeeDashboard />} />
+			<Route path='/HoneyCombHR/addEmployee' element={<AddEmployee />} />
 		</Routes>
 	)
 }
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BumbleBeeHR/',
+  base: '/HoneyCombHR/',
   plugins: [react()],
   server: {
     port: 3000
